1a3ba167876a56eddd2c9188a60cada84f47dc5b
ajikameasane-eng <ajikameasane@gmail.com>
2025-11-05 22:48:52 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,1 +1,16 @@
-
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: '#0b0b0f',
+        card: '#0f1116',
+        accent: '#ffd166'
+      },
+      fontFamily: {
+        uvn: ['"UVN-VAN"', 'Inter', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
