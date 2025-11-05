6ed9dd07cde9842dffe1980ac23bb96af2adc108
ajikameasane-eng <ajikameasane@gmail.com>
2025-11-05 22:09:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+import { db } from "./firebase.js";
+import { ref, push, set } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
+
+document.getElementById("uploadForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  push(ref(db, "stories"), {
+    title,
+    content,
+    date: new Date().toISOString()
+  });
+
+  alert("Đăng truyện thành công!");
+  e.target.reset();
+});
